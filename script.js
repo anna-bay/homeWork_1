15e9aa815edd5b422c8b1a60807f04f76d34d1ce
Anna <anna.bay@list.ru>
2020-07-15 22:39:00 +0400

    * to +

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let a = prompt("Please enter Number_1")
 let b = prompt("Please enter Number_2")
 
-let c = parseInt(a) * parseInt(b)
+let c = parseInt(a) + parseInt(b)
 
 
-alert(`Number_1 * Number_2 is ${c}`)
+alert(`Number_1 + Number_2 is ${c}`)
